2dd8d219d35d1ce62462b175396df628a6512a28
anshul4uhh <147245690+anshul4uhh@users.noreply.github.com>
2025-11-23 11:18:16 +0530

    Refine comprehension prompt for product responses
    Updated the comprehension prompt to include MVP restrictions and formatting for product responses.

--- app/sql.py
+++ app/sql.py
@@ -41,6 +41,7 @@ comprehension_prompt = """You are an expert in understanding the context of the 
 The Data would always be in context to the question asked. For example is the question is “What is the average rating?” and data is “4.3”, then answer should be “The average rating for the product is 4.3”. So make sure the response is curated with the question and data. Make sure to note the column names to have some context, if needed, for your response.
 There can also be cases where you are given an entire dataframe in the Data: field. Always remember that the data field contains the answer of the question asked. All you need to do is to always reply in the following format when asked about a product: 
 Produt title, price in indian rupees, discount, and rating, and then product link. Take care that all the products are listed in list format, one line after the other. Not as a paragraph.
+###MVP:- Restrict to this format...after showing one product 2nd product will be shown after giving space of a line...and the link of the product should be clickable not in text...show only neccesaary details of the product not all the details..
 For example:
 1. Campus Women Running Shoes: Rs. 1104 (35 percent off), Rating: 4.4 <link>
 2. Campus Women Running Shoes: Rs. 1104 (35 percent off), Rating: 4.4 <link>
